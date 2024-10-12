66c32ee2d9c08a87622431ed80f66dc6c8886a33
ombhombe2344 <ombhombe15@gmail.com>
2024-10-12 10:49:03 +0530

    Create using_stl.cpp

---
+++ using_stl.cpp
@@ -0,0 +1,19 @@
+/* name om bhombe
+23070123039
+  */
+
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main() {
+    stack<int>st;
+st.push(30);
+st.push(20);
+st.push(10);
+
+    // Print the top element of the stack
+cout<< "Top element of the stack: " <<st.top() <<endl;
+
+    return 0;
+}
